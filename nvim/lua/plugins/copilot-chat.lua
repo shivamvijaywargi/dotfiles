e6416835c4c2df321e9789a36fde75dabd5213e7
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-11-10 13:03:19 +0530

    feat: add key mappings for model list, selection✨

---
+++ nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,7 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  keys = {
+    { "<leader>am", "<cmd>CopilotChatModel<cr>", desc = "Show current Copilot Chat Model" },
+    { "<leader>as", "<cmd>CopilotChatModels<cr>", desc = "Change Copilot Chat Model" },
+  },
+}
