9fdb1ea4a2456d83cd96f8919a51afafb773c837
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2025-02-04 23:03:25 +0530

    feat: replace telescope/fzf with snacks picker✨

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,44 +1,45 @@
-return {
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = function()
-      return {
-        defaults = {
-          require("telescope").setup({
-            pickers = {
-              find_files = {
-                find_command = { "rg", "--files", "--sortr=modified" },
-              },
-            },
-          }),
-          -- When I search for stuff in telescope, I want the path to be shown
-          -- first, this helps in files that are very deep in the tree and I
-          -- cannot see their name.
-          -- Also notice the "reverse_directories" option which will show the
-          -- closest dir right after the filename
-          path_display = {
-            filename_first = {
-              reverse_directories = true,
-            },
-          },
-        },
-      }
-    end,
-    keys = {
-      { "<leader>fz", LazyVim.pick("auto", { root = false }), desc = "Find Files (Root Dir)" },
-      { "<leader>fF", "<cmd>Telescope frecency<cr>", desc = "Find Files (cwd)" },
-      {
-        "<leader>ff",
-        "<cmd>Telescope frecency workspace=CWD theme=ivy<cr>",
-        desc = "Find Files (Root Dir)",
-      },
-      { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (cwd)" },
-      { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (Root Dir)" },
-      {
-        "<leader><space>",
-        "<cmd>Telescope frecency workspace=CWD theme=ivy<cr>",
-        desc = "Find Files (Root Dir)",
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   {
+--     "nvim-telescope/telescope.nvim",
+--     opts = function()
+--       return {
+--         defaults = {
+--           require("telescope").setup({
+--             pickers = {
+--               find_files = {
+--                 find_command = { "rg", "--files", "--sortr=modified" },
+--               },
+--             },
+--           }),
+--           -- When I search for stuff in telescope, I want the path to be shown
+--           -- first, this helps in files that are very deep in the tree and I
+--           -- cannot see their name.
+--           -- Also notice the "reverse_directories" option which will show the
+--           -- closest dir right after the filename
+--           path_display = {
+--             filename_first = {
+--               reverse_directories = true,
+--             },
+--           },
+--         },
+--       }
+--     end,
+--     keys = {
+--       { "<leader>fz", LazyVim.pick("auto", { root = false }), desc = "Find Files (Root Dir)" },
+--       { "<leader>fF", "<cmd>Telescope frecency<cr>", desc = "Find Files (cwd)" },
+--       {
+--         "<leader>ff",
+--         "<cmd>Telescope frecency workspace=CWD theme=ivy<cr>",
+--         desc = "Find Files (Root Dir)",
+--       },
+--       { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (cwd)" },
+--       { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (Root Dir)" },
+--       {
+--         "<leader><space>",
+--         "<cmd>Telescope frecency workspace=CWD theme=ivy<cr>",
+--         desc = "Find Files (Root Dir)",
+--       },
+--     },
+--   },
+-- }
