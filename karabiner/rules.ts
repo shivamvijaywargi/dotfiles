3327c6741824a922946f947146d53659c808b97b
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2025-02-12 21:47:08 +0530

    feat: add karabiner config

---
+++ karabiner/rules.ts
@@ -0,0 +1,242 @@
+import fs from "fs";
+import { KarabinerRules } from "./types";
+import { createHyperSubLayers, app, open, rectangle, shell } from "./utils";
+
+const rules: KarabinerRules[] = [
+  // Define the Hyper key itself
+  {
+    description: "Hyper Key (⌃⌥⇧⌘)",
+    manipulators: [
+      {
+        description: "Caps Lock -> Hyper Key",
+        from: {
+          key_code: "caps_lock",
+          modifiers: {
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            set_variable: {
+              name: "hyper",
+              value: 1,
+            },
+          },
+        ],
+        to_after_key_up: [
+          {
+            set_variable: {
+              name: "hyper",
+              value: 0,
+            },
+          },
+        ],
+        to_if_alone: [
+          {
+            key_code: "escape",
+          },
+        ],
+        type: "basic",
+      },
+      //      {
+      //        type: "basic",
+      //        description: "Disable CMD + Tab to force Hyper Key usage",
+      //        from: {
+      //          key_code: "tab",
+      //          modifiers: {
+      //            mandatory: ["left_command"],
+      //          },
+      //        },
+      //        to: [
+      //          {
+      //            key_code: "tab",
+      //          },
+      //        ],
+      //      },
+    ],
+  },
+  ...createHyperSubLayers({
+    // b = "B"rowse
+    b: {
+      g: open("https://github.com"),
+      r: open("https://reddit.com"),
+      y: open("https://youtube.com"),
+      p: open("https://shivamvijaywargi.dev"),
+    },
+    // o = "Open" applications
+    o: {
+      a: app("Arc"),
+      d: app("Discord"),
+      g: app("Ghostty"),
+      t: app("iTerm"),
+      v: app("Visual Studio Code"),
+      z: app("Zed"),
+      f: app("Finder"),
+      // "W"hatsApp has been replaced by Texts
+      w: open("WhatsApp"),
+    },
+
+    // TODO: This doesn't quite work yet.
+    // l = "Layouts" via Raycast's custom window management
+    // l: {
+    //   // Coding layout
+    //   c: shell`
+    //     open -a "Visual Studio Code.app"
+    //     sleep 0.2
+    //     open -g "raycast://customWindowManagementCommand?position=topLeft&relativeWidth=0.5"
+
+    //     open -a "Terminal.app"
+    //     sleep 0.2
+    //     open -g "raycast://customWindowManagementCommand?position=topRight&relativeWidth=0.5"
+    //   `,
+    // },
+
+    // s = "System"
+    s: {
+      u: {
+        to: [
+          {
+            key_code: "volume_increment",
+          },
+        ],
+      },
+      j: {
+        to: [
+          {
+            key_code: "volume_decrement",
+          },
+        ],
+      },
+      i: {
+        to: [
+          {
+            key_code: "display_brightness_increment",
+          },
+        ],
+      },
+      k: {
+        to: [
+          {
+            key_code: "display_brightness_decrement",
+          },
+        ],
+      },
+      l: {
+        to: [
+          {
+            key_code: "q",
+            modifiers: ["right_control", "right_command"],
+          },
+        ],
+      },
+      p: {
+        to: [
+          {
+            key_code: "play_or_pause",
+          },
+        ],
+      },
+      semicolon: {
+        to: [
+          {
+            key_code: "fastforward",
+          },
+        ],
+      },
+      // "D"o not disturb toggle
+      d: open(
+        `raycast://extensions/yakitrak/do-not-disturb/toggle?launchType=background`
+      ),
+      c: open("raycast://extensions/raycast/system/open-camera"),
+    },
+
+    // v = "moVe" which isn't "m" because we want it to be on the left hand
+    // so that hjkl work like they do in vim
+    v: {
+      h: {
+        to: [{ key_code: "left_arrow" }],
+      },
+      j: {
+        to: [{ key_code: "down_arrow" }],
+      },
+      k: {
+        to: [{ key_code: "up_arrow" }],
+      },
+      l: {
+        to: [{ key_code: "right_arrow" }],
+      },
+      // Magicmove via homerow.app
+      m: {
+        to: [{ key_code: "f", modifiers: ["right_control"] }],
+        // TODO: Trigger Vim Easymotion when VSCode is focused
+      },
+      // Scroll mode via homerow.app
+      s: {
+        to: [{ key_code: "j", modifiers: ["right_control"] }],
+      },
+      d: {
+        to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
+      },
+      u: {
+        to: [{ key_code: "page_down" }],
+      },
+      i: {
+        to: [{ key_code: "page_up" }],
+      },
+    },
+
+    // c = Musi*c* which isn't "m" because we want it to be on the left hand
+    c: {
+      p: {
+        to: [{ key_code: "play_or_pause" }],
+      },
+      n: {
+        to: [{ key_code: "fastforward" }],
+      },
+      b: {
+        to: [{ key_code: "rewind" }],
+      },
+    },
+
+    // r = "Raycast"
+    r: {
+      c: open("raycast://extensions/thomas/color-picker/pick-color"),
+      e: open(
+        "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"
+      ),
+      g: open("raycast://extensions/ricoberger/gitmoji/gitmoji"),
+      p: open("raycast://extensions/raycast/raycast/confetti"),
+      s: open("raycast://extensions/peduarte/silent-mention/index"),
+      h: open(
+        "raycast://extensions/raycast/clipboard-history/clipboard-history"
+      ),
+      1: open(
+        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-1"
+      ),
+      2: open(
+        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-2"
+      ),
+    },
+  }),
+];
+
+fs.writeFileSync(
+  "karabiner.json",
+  JSON.stringify(
+    {
+      global: {
+        show_in_menu_bar: false,
+      },
+      profiles: [
+        {
+          name: "Default",
+          complex_modifications: {
+            rules,
+          },
+        },
+      ],
+    },
+    null,
+    2
+  )
+);
